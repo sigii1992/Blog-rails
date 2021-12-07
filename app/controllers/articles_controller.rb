32d0ad78070814212ea27f20ca35a5a8e522663f
Sigita <sigita.zeb92@gmail.com>
2021-12-07 13:34:43 +0000

    Add new and create actions to the articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,21 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show 
+  def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
 end
