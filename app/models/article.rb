99a165a7dc2291d51a678b96cdaf2b7895acd554
Sigita <sigita.zeb92@gmail.com>
2021-12-07 16:29:28 +0000

    Add basic authentication

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,11 @@
 class Article < ApplicationRecord
   include Visible
-  has_many :comments
+
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
+
 
 
