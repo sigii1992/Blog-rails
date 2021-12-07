f4cb0c4e9d5bb0b7f145f4db4b37203e4de0f108
Sigita <sigita.zeb92@gmail.com>
2021-12-07 16:12:05 +0000

    use concersns

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,10 +41,10 @@ class ArticlesController < ApplicationController
 
     redirect_to root_path
   end
-  
+
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
 
